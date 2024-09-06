c2800410ec01fc6341b42202fc4b26136515a15a
Naitais <enzo.f9849@gmail.com>
2024-09-06 18:58:14 -0300

    creo tostring hashcode y equals functions para mi clase clientes

--- homebanking/src/main/java/practicas2/homebanking/clientes/Clientes.java
+++ homebanking/src/main/java/practicas2/homebanking/clientes/Clientes.java
@@ -1,5 +1,7 @@
 package practicas2.homebanking.clientes;
 
+import java.util.Objects;
+
 public record Clientes(
         Integer id,
         String nombre,
@@ -7,4 +9,25 @@ public record Clientes(
 
 
 ) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Clientes clientes = (Clientes) o;
+        return Objects.equals(id, clientes.id) && Objects.equals(nombre, clientes.nombre) && Objects.equals(apellido, clientes.apellido);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nombre, apellido);
+    }
+
+    @Override
+    public String toString() {
+        return "Clientes{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                '}';
+    }
 }
