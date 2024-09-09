4a0a8211625e21688a223dbad64183a455e88800
Naitais <enzo.f9849@gmail.com>
2024-09-09 16:03:53 -0300

    agrego relacion de uno a muchos en cliente cuentas falta armar controlador y repositorio para clase cuentas

--- homebanking/src/main/java/practicas2/homebanking/clientes/Clientes.java
+++ homebanking/src/main/java/practicas2/homebanking/clientes/Clientes.java
@@ -1,11 +1,13 @@
 package practicas2.homebanking.clientes;
 
-
+import java.util.ArrayList;
+import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import org.hibernate.annotations.GenericGenerator;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import practicas2.homebanking.cuentas.Cuentas;
 
 @Entity
 public class Clientes {
@@ -18,7 +20,29 @@ public class Clientes {
     private String apellido;
     private String email;
 
-    public Clientes(String nombre, String apellido, String email) {
+    // Relaciono mi entidad cliente con cuentas:
+
+    // mappedBy = "cliente" -> significa que la entidad cuentas va a tener una clave foranea cliente
+
+    // cascade = CascadeType.ALL -> significa que cualquier modificacion que afecte a clientes afecta
+    // a todas las cuentas asociadas
+    /*
+    * PERSIST: When the parent entity is persisted (saved), the child entities are also persisted.
+      MERGE: When the parent entity is merged (updated), the child entities are also merged.
+      REMOVE: When the parent entity is removed (deleted), the child entities are also removed.
+      REFRESH: When the parent entity is refreshed, the child entities are also refreshed.
+      DETACH: When the parent entity is detached from the persistence context, the child entities are also detached.
+      ALL: This is shorthand for applying all the above operations (i.e., persist, merge, remove, refresh, detach).
+    * */
+
+    //fetch = FetchType.LAZY -> significa que los datos de la cuenta asociada se buscan o cargan
+    // solo cuando se acceden via codigo. Además de LAZY esta EAGER que hace que se carguen inmediatamente
+    // dependiendo del caso usar LAZY ayuda a la performance porque no carga de forma innecesaria
+
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Cuentas> cuentas = new ArrayList<Cuentas>();
+
+    public Clientes(String nombre, String apellido, String email, ArrayList<Cuentas> cuentas) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.email = email;
@@ -56,5 +80,13 @@ public class Clientes {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Cuentas> getCuentas() {
+        return cuentas;
+    }
+
+    public void setCuentas(List<Cuentas> cuentas) {
+        this.cuentas = cuentas;
     }
 }
---
+++ homebanking/src/main/java/practicas2/homebanking/cuentas/Cuentas.java
@@ -0,0 +1,67 @@
+package practicas2.homebanking.cuentas;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+import practicas2.homebanking.clientes.Clientes;
+
+import java.time.LocalDate;
+
+@Entity
+public class Cuentas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+    private String numero;
+    private LocalDate fechaCreacion;
+    private double saldo;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Clientes cliente;
+
+
+    public Cuentas(int id, String numero, LocalDate fechaCreacion, double saldo) {
+        this.id = id;
+        this.numero = numero;
+        this.fechaCreacion = fechaCreacion;
+        this.saldo = saldo;
+    }
+
+    public Cuentas(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public LocalDate getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDate fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+
+}
